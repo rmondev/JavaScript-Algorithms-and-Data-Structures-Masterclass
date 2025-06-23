5c242826fba92a0ec31c9d2bb6e0c3451c9257d3
Riccardo Moncada <rmondev@gmail.com>
2025-06-23 14:16:20 -0400

    Implement maxSubarraySum function and add console test for maximum subarray sum calculation

---
+++ 34 Sliding Window Pattern/script2.js
@@ -0,0 +1,24 @@
+
+function maxSubarraySum(arr, n) {
+    let tempSum = 0;
+    let maxSum = 0;
+
+    for(let i = 0; i < n; i++) {
+        maxSum += arr[i];
+    }
+
+    tempSum = maxSum;
+
+    for(let i = n; i < arr.length; i++) {
+        tempSum = tempSum - (arr[i-n]) /** subtract value from previous index and add value in next index */ + arr[i];
+
+        if (tempSum > maxSum){
+            maxSum = tempSum;
+        }
+    }
+
+    return maxSum;
+}
+
+
+console.log(maxSubarraySum([2,6,9,2,1,8,5,6,3],3));
