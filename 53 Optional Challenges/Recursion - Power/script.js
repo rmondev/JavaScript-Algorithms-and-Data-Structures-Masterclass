cc6a9fef5e590239ab9bb78101cfd1f4fc7e99b3
Riccardo Moncada <rmondev@gmail.com>
2025-06-30 22:04:34 -0400

    Add recursive functions for collecting odd values, calculating factorial, and computing power

---
+++ 53 Optional Challenges/Recursion - Power/script.js
@@ -0,0 +1,13 @@
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(base, exp){
+    if (exp === 0 ) return 1;
+    return base * power(base, exp-1)
+}
+
+console.log(power(2,4))
+console.log(power(2,2))
+console.log(power(2,0))
+
